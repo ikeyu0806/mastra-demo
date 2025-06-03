5417bfe48fbb178085ee05ae52b248d922bb5569
ikeyu0806 <ikeyu0806@gmail.com>
2025-06-03 16:56:23 +0900

    add mcp agent

--- src/mastra/agents/mcp-agent.ts
+++ src/mastra/agents/mcp-agent.ts
@@ -2,7 +2,7 @@ import { mcp } from "../mcp";
 import { Agent } from "@mastra/core/agent";
 import { openai } from "@ai-sdk/openai";
 // Create an agent and add tools from the MCP client
-const agent = new Agent({
+export const mcpAgent = new Agent({
   name: "Agent with MCP Tools",
   instructions: "You can use tools from connected MCP servers.",
   model: openai("gpt-4o-mini"),
--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -4,9 +4,10 @@ import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 
 import { weatherAgent } from './agents/weather-agent';
+import { mcpAgent } from './agents/mcp-agent';
 
 export const mastra = new Mastra({
-  agents: { weatherAgent },
+  agents: { weatherAgent, mcpAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
