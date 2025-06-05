27467389e3157ac474d3313b7629daa6dc0d9e30
ikeyu0806 <ikeyu0806@gmail.com>
2025-06-05 16:49:59 +0900

    fix loadfile

--- src/mastra/tools/faqFromFile.ts
+++ src/mastra/tools/faqFromFile.ts
@@ -1,7 +1,12 @@
 import * as fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
 import { createTool } from '@mastra/core/tools'
 import { z } from 'zod'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export const faqFromFile = createTool({
   id: 'Get Faq Information',
@@ -10,7 +15,7 @@ export const faqFromFile = createTool({
   }),
   description: `Fetches the FAQ information from a file`,
   execute: async () => {
-    const filePath = '../../mastra/texts/faq.txt'
+    const filePath = join(__dirname, '../../src/mastra/texts/faq.txt')
     const contents = fs.readFileSync(filePath, 'utf8')
 
     return {
